6cb109612b820fe2282b492bc86c21f261070b92
drzln <drzzln@protonmail.com>
2025-09-14 02:28:10 -0300

    init

--- flake.nix
+++ flake.nix
@@ -6,21 +6,24 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            docker-compose
-          ];
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells.default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          docker-compose
+        ];
 
-          shellHook = ''
-            echo "Curupira development environment"
-            echo "docker-compose is available"
-          '';
-        };
-      });
+        shellHook = ''
+          echo "Curupira development environment"
+          echo "docker-compose is available"
+        '';
+      };
+    });
 }
+
